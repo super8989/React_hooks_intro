f6df35421fcac608975f951d7f343f700aa25966
Sam <super8989@gmail.com>
2019-12-25 18:57:02 -0500

    useState with hooks

--- src/CounterHooks.js
+++ src/CounterHooks.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CounterHooks() {
+	const [count, setCount] = useState(0);
+	//1. the piece of state
+	//2. function to update that piece of state
+	//3. initial value
+
 	return (
 		<div>
-			<h1>The Count is: </h1>
-			<button>Add 1</button>
+			<h1>The Count is: {count} </h1>
+			<button onClick={() => setCount(count + 1)}>Add 1</button>
 		</div>
 	);
 }
