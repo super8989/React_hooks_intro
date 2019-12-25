ec529fa5c784a4d2aa13a7e736912202acad3901
Sam <super8989@gmail.com>
2019-12-25 16:43:19 -0500

    added save to local storage

--- src/CounterClass.js
+++ src/CounterClass.js
@@ -3,14 +3,20 @@ import React, { Component } from "react";
 class CounterClass extends Component {
 	constructor(props) {
 		super(props);
+		const savedCount = JSON.parse(window.localStorage.getItem("count"));
+
 		this.state = {
-			count: 0
+			count: savedCount || 0
 		};
 		this.increment = this.increment.bind(this);
 	}
 
 	increment() {
-		this.setState({ count: this.state.count + 1 });
+		this.setState({ count: this.state.count + 1 }, this.syncLocalStorage);
+	}
+
+	syncLocalStorage() {
+		window.localStorage.setItem("count", JSON.stringify(this.state.count));
 	}
 
 	render() {
