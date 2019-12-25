ebb31a4931de2f5673a29168489aa9c7dc3942d4
Sam <super8989@gmail.com>
2019-12-25 16:31:20 -0500

    default skeleton for CounterClass

--- src/CounterClass.js
+++ src/CounterClass.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
 class CounterClass extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: 0
+		};
+	}
+
 	render() {
 		return (
 			<div>
-				<h1>The Count Is: </h1>
+				<h1>The Count Is: {this.state.count} </h1>
 				<button>Add 1</button>
 			</div>
 		);
