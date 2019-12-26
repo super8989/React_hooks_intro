3cdba84f30e3580694720dda4e99e9babc32db9a
Sam <super8989@gmail.com>
2019-12-25 19:35:05 -0500

    added another toggle for another state

--- src/Toggler.js
+++ src/Toggler.js
@@ -2,11 +2,22 @@ import React, { useState } from "react";
 
 function Toggler() {
 	const [isHappy, setIsHappy] = useState(true);
-	const toggle = () => {
+	const [isHeartbroken, setIsHeartbroken] = useState(false);
+
+	const toggleIsHappy = () => {
 		setIsHappy(!isHappy);
 	};
 
-	return <h1 onClick={toggle}>{isHappy ? "😆" : "😭"}</h1>;
+	const toggleIsHeartbroken = () => {
+		setIsHeartbroken(!isHeartbroken);
+	};
+
+	return (
+		<div>
+			<h1 onClick={toggleIsHappy}>{isHappy ? "😆" : "😭"}</h1>
+			<h1 onClick={toggleIsHeartbroken}>{isHeartbroken ? "💔" : "💖"} </h1>
+		</div>
+	);
 }
 
 export default Toggler;
