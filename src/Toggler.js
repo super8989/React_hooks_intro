3a8cfd7c4368887d82a14c37f3def59d3dbd7281
Sam <super8989@gmail.com>
2019-12-25 19:24:32 -0500

    toggle using a function

--- src/Toggler.js
+++ src/Toggler.js
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 
 function Toggler() {
 	const [isHappy, setIsHappy] = useState(true);
+	const toggle = () => {
+		setIsHappy(!isHappy);
+	};
 
-	return <h1 onClick={() => setIsHappy(!isHappy)}>{isHappy ? "😆" : "😭"}</h1>;
+	return <h1 onClick={toggle}>{isHappy ? "😆" : "😭"}</h1>;
 }
 
 export default Toggler;
