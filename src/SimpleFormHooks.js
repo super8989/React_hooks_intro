6ccd6853e4a1ded5a5cf9e6a49df946150e73336
Sam <super8989@gmail.com>
2019-12-27 11:04:57 -0500

    traditional react form

--- src/SimpleFormHooks.js
+++ src/SimpleFormHooks.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 
 function SimpleFormHooks() {
-	const [email, setEmail] = useState("hello");
+	const [email, setEmail] = useState("");
+	const handleChange = e => {
+		setEmail(e.target.value);
+	};
 
 	return (
 		<div>
-			<h1>The value is ..</h1>
-			<input type='text' value={email} />
+			<h1>The value is ... {email}</h1>
+			<input type='text' value={email} onChange={handleChange} />
+			<button onClick={() => setEmail("")}>Submit</button>
 		</div>
 	);
 }
