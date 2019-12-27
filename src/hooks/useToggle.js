2aed37ca69053a115047b8287aa55a9d043a854c
Sam <super8989@gmail.com>
2019-12-27 11:13:47 -0500

    created a useFormState hook to use in SimpleFormHooks.js

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function useToggle(initialVal = false) {
 	// call useState, "reserve" piece of state
 	const [state, setState] = useState(initialVal);
+
 	const toggle = () => {
 		setState(!state);
 	};
